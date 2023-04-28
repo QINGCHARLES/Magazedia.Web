6a8e1a2f513eab57b0e195b0b95a86cab58940ef
Mark Harmon <neothej1sec@gmail.com>
2023-04-28 15:18:45 -0500

    First

---
+++ Magazedia.Web/Data/Create.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE AspNetRoles( Id nvarchar(450) NOT NULL PRIMARY KEY, ConcurrencyStamp nvarchar(max) NULL, [Name] nvarchar(256) NULL, NormalizedName nvarchar(256) NULL );
+CREATE TABLE AspNetRoleClaims( Id int IDENTITY(1,1) NOT NULL PRIMARY KEY, ClaimType nvarchar(max) NULL, ClaimValue nvarchar(max) NULL, RoleId nvarchar(450) NOT NULL );
+CREATE TABLE AspNetUsers( Id nvarchar(450) NOT NULL PRIMARY KEY, AccessFailedCount int NOT NULL, ConcurrencyStamp nvarchar(max) NULL, Email nvarchar(256) NULL, EmailConfirmed bit NOT NULL, LockoutEnabled bit NOT NULL, LockoutEnd datetimeoffset(7) NULL, NormalizedEmail nvarchar(256) NULL, NormalizedUserName nvarchar(256) NULL, PasswordHash nvarchar(max) NULL, PhoneNumber nvarchar(max) NULL, PhoneNumberConfirmed bit NOT NULL, SecurityStamp nvarchar(max) NULL, TwoFactorEnabled bit NOT NULL, UserName nvarchar(256) NULL );
+CREATE TABLE AspNetUserClaims( Id int IDENTITY(1,1) NOT NULL PRIMARY KEY, ClaimType nvarchar(max) NULL, ClaimValue nvarchar(max) NULL, UserId nvarchar(450) NOT NULL );
+CREATE TABLE AspNetUserLogins( LoginProvider nvarchar(450) NOT NULL, ProviderKey nvarchar(450) NOT NULL, ProviderDisplayName nvarchar(max) NULL, UserId nvarchar(450) NOT NULL, CONSTRAINT PK_AspNetUsersLogin PRIMARY KEY( LoginProvider, ProviderKey ) );
+CREATE TABLE AspNetUserRoles( UserId nvarchar(450) NOT NULL, RoleId nvarchar(450) NOT NULL, CONSTRAINT PK_AspNetUsersRole PRIMARY KEY( UserId, RoleId ) );
+CREATE TABLE AspNetUserTokens( UserId nvarchar(450) NOT NULL, LoginProvider nvarchar(450) NOT NULL, [Name] nvarchar(450) NOT NULL, [Value] nvarchar(max) NULL, CONSTRAINT PK_AspNetUsersToken PRIMARY KEY( UserId, LoginProvider, [Name] ) );
+GO
