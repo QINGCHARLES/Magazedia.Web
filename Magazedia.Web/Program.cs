51fe05c3b04a7508403266cdd1ef93f062c9df2d
Mark Harmon <neothej1sec@gmail.com>
2023-04-28 15:39:35 -0500

    a

--- Magazedia.Web/Program.cs
+++ Magazedia.Web/Program.cs
@@ -28,7 +28,37 @@ else
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+using (TextReader sr = new StringReader(@$"
+    <rewrite>
+        <rules>
+            <clear />
+            <rule enabled=""true"" stopProcessing=""true"">
+                <match url=""(.*)"" />
+                <conditions logicalGrouping=""MatchAll"" trackAllCaptures=""false"">
+                    <add input=""{{HTTPS}}"" pattern=""^OFF$"" />
+                </conditions>
+                <action type=""Redirect"" url=""https://magazedia.site{{REQUEST_URI}}"" appendQueryString=""false"" redirectType=""308"" />
+            </rule>
+            <rule enabled=""true"">
+                <match url=""(.*)"" />
+                <conditions logicalGrouping=""MatchAll"" trackAllCaptures=""false"">
+                    <add input=""{{HTTP_HOST}}"" pattern=""^magazedia\.site$|^ja.magazedia.site$"" negate=""true"" />
+                </conditions>
+                <action type=""Redirect"" url=""https://magazedia.site/{{R:1}}"" redirectType=""308"" />
+            </rule>
+        </rules>
+    </rewrite>
+"))
+{
+        var options = new RewriteOptions()
+                .AddIISUrlRewrite(sr)
+                .AddRewrite(@"^(?!.*\.(?:jpg|jpeg|gif|png|bmp|css|js)$)(.*)", "Article?UrlSlug=$1", skipRemainingRules: true);
+//              .AddRewrite(@"^(.*)", "Article?title=$1", skipRemainingRules: true);
+		app.UseRewriter(options);
+}
+
+
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
