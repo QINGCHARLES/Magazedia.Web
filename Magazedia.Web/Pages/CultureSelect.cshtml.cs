7ceffa20f0595d67adc6063f32487868aae966b8
QINGCHARLES <kingcharlesother@gmail.com>
2023-09-27 00:15:47 -0500

    27sep23a

--- Magazedia.Web/Pages/CultureSelect.cshtml.cs
+++ Magazedia.Web/Pages/CultureSelect.cshtml.cs
@@ -2,17 +2,16 @@ using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
+using System.Globalization;
 
 namespace Magazedia.Web.Pages
 {
     public class CultureSelectModel : BasePageModel
     {
         public string[] Cultures = { "en", "eu", "ru", "fr", "es", "ja", "ar" };
-        public Dictionary<string, int> CultureCount = new Dictionary<string, int>();
+        public Dictionary<string, string> CultureCount = new ();
 
-        public CultureSelectModel(IConfiguration Configuration, IHttpContextAccessor HttpContextAccessor) : base(Configuration, HttpContextAccessor)
-        {
-        }
+        public CultureSelectModel(IConfiguration Configuration, IHttpContextAccessor HttpContextAccessor) : base(Configuration, HttpContextAccessor) { }
 
         public void OnGet()
         {
@@ -26,11 +25,22 @@ namespace Magazedia.Web.Pages
             using SqlConnection Connection = new(Configuration.GetConnectionString("DefaultConnection"));
             int SiteId = 1;
 
-            var results = Connection.Query(SqlQuery, new { SiteId }).ToDictionary(x => (string)x.Culture, x => (int)x.Count);
+            Dictionary<string, int> CulturesArticlesCounts = Connection.Query(SqlQuery, new { SiteId }).ToDictionary(x => (string)x.Culture, x => (int)x.Count);
 
-            foreach (var Culture in Cultures)
+            foreach (string Culture in Cultures)
             {
-                CultureCount[Culture] = results.ContainsKey(Culture) ? results[Culture] : 0;
+                int ArticleCount = CulturesArticlesCounts.ContainsKey(Culture) ? CulturesArticlesCounts[Culture] : 0;
+
+                // Create a new NumberFormatInfo object
+                NumberFormatInfo NumberFormatInfo = new CultureInfo("en-US", false).NumberFormat;
+
+                // Set the thousand separator to a space
+                NumberFormatInfo.NumberGroupSeparator = " ";
+
+                // Use the custom format info with the ToString method
+                string FormattedArticleCount = ArticleCount.ToString("#,0", NumberFormatInfo);
+
+                CultureCount[Culture] = FormattedArticleCount;
             }
         }
     }
